b1534fd0ea1445e1e3dd3d2af170d1566ea1faeb
vyaron <vyaron@gmail.com>
2023-03-08 16:42:51 +0200

    remove the / route

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json())
 app.use(express.static('public'))
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!!')
+// })
 
 app.get('/puki', (req, res) => {
     var visitCount = req.cookies.visitCount || 0
